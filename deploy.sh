8f00ebbe7a785366a966f75f3bd17b29f69c8a66
Yuval Timen <yuvaltimen@gmail.com>
2025-04-04 16:15:27 -0400

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+docker build --platform linux/amd64 -t yuvaltimen/onewordstory . 
+
+docker push yuvaltimen/onewordstory 
+
 gcloud run deploy onewordstory \
   --image=docker.io/yuvaltimen/onewordstory \
   --platform=managed \
